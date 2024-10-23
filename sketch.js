8b62624bbd97741173dfb70307ce4509d6f46cd8
02aob20241H <anelise.brito@escola.pr.gov.br>
2024-10-23 14:26:17 -0300

    Adicionando Arquivos JS E CSS

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+    // cria uma tela de 400px de altura por 400px de largura
+    createCanvas(400, 400);
+    background("white")
+  }
+  
+  function draw() {
+    stroke("blue");
+    fill("red");
+  
+    
+    if (mouseIsPressed) {
+      rect(mouseX, mouseY, 20, 35);
+    }
+  }
